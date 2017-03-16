54d146c667fbf764d87a98595cb5dfc331f0a15a
大森　亮佑 <s1210130@gmail.com>
2017-03-17 00:35:52 +0900

    [add] PhotographViewのxibファイルとSwiftファイルを追加

---
+++ VideoDemo/PhotographView.swift
@@ -0,0 +1,14 @@
+//
+//  PhotographView.swift
+//  VideoDemo
+//
+//  Created by 大森　亮佑 on 2017/03/17.
+//  Copyright © 2017年 RyosukeOmori. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class PhotographView : UIView {
+    
+}
